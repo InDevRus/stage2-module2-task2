a7304a608b2ce15123e9db3b170f7d01ac5f9e42
indev <InDevRus@hotmail.com>
2023-08-18 15:55:09 +0000

    Implemented AuthFilter

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,28 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import com.example.jsp.paths.JSPPaths;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.util.Objects;
+
+@WebFilter(urlPatterns = "/user/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        if (Objects.isNull(servletRequest.getAttribute("user"))) {
+            servletRequest.getRequestDispatcher(JSPPaths.LOGIN).forward(servletRequest, servletResponse);
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 }

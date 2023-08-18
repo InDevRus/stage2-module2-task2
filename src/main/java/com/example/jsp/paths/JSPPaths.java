d983b0cf2aade05ea697683f50d5d2462cd78fb8
indev <InDevRus@hotmail.com>
2023-08-18 15:55:54 +0000

    Hard coded JSP Paths into constants

---
+++ src/main/java/com/example/jsp/paths/JSPPaths.java
@@ -0,0 +1,10 @@
+package com.example.jsp.paths;
+
+public final class JSPPaths {
+    private JSPPaths() {
+    }
+
+    public static final String HELLO = "/user/hello.jsp";
+    public static final String INDEX = "index.jsp";
+    public static final String LOGIN = "login.jsp";
+}
